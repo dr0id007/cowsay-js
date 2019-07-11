8d5cedeae87f659ef9ea01a24f0b7e605f817475
dr0id007 <nullvoid.nv@gmail.com>
2019-07-11 08:52:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const cowsay = require("cowsay");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api/cow/:text", (req, res) => {
+  const { text } = req.params;
+  console.log("api request");
+  console.log(text);
+  const result = cowsay.say({
+    text: text
+  });
+  return res.send(result);
+});
+
+app.get("/", (req, res) => {
+  console.log("get request");
+  return res.send(
+    cowsay.say({
+      text: "Hello World.!"
+    })
+  );
+});
+
+app.listen(4000);
